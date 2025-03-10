fded743781352124713333934a8232e94943f1d1
Daniel SDLF <daniel.sosadelafuente@education.nsw.gov.au>
2025-03-10 01:50:53 +0000

    add index

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+player_name = prompt("enter your name");
+alert("Hello " + player_name);
+player_guess = prompt("Rock, Paper or Scissors?");
+computer_guess = randomInteger(1, 3);
+if (player_guess == computer_guess) {
+  document.getElementById("user_feedback").innerHTML =
+    "<em>Correct, You WIN</em>";
+} else {
+  document.getElementById("user_feedback").innerHTML =
+    "Incorrect, you LOSE, dummy\n" + "The computer guessed " + computer_guess;
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
