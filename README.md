bfb47141beb757eb5e7a5f8a47166d0f599ff7ff
Daniel SDLF <daniel.sosadelafuente@education.nsw.gov.au>
2025-06-22 23:40:50 +0000

    add ttf files and rac.html

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 git add -A
-git commit -m "add ttf files and #fart"
+git commit -m "add ttf files and rac.html"
 git push
--- main.py
+++ main.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("Title", "Description", "Button text", "static/images/card1.png"),
-        ("Title", "Description", "Button text", "static/images/card2.png"),
-        ("Title", "Description", "Button text", "static/images/card3.png"),
-        ("Title", "Description", "Button text", "static/images/card4.png"),
+        ("Title", "Description", "Button text", "static/images/CARDONE.png"),
+        ("Title", "Description", "Button text", "static/images/CARDTWO.png"),
+        ("Title", "Description", "Button text", "static/images/CARDTHREE.png"),
+        ("Title", "Description", "Button text", "static/images/CARDFOUR.png"),
     )
     return render_template("index.html", cards=card_data), 200
 
@@ -18,6 +18,13 @@ def index():
 @app.route('/contact.html')
 def contact():
     return render_template("contact.html"), 200
+
+
+@app.route('/RAC.html')
+def RAC():
+    return render_template("RAC.html"), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
